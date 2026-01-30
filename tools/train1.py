aaa945a58ee59b34487f251ba3a3143159481225
Dingcong206 <dingcong882@gmail.com>
2026-01-30 17:45:44 +0900

    D

--- tools/train1.py
+++ tools/train1.py
@@ -6,8 +6,6 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(PROJECT_ROOT))
 
-print("PROJECT_ROOT =", PROJECT_ROOT)
-print("sys.path[0]  =", sys.path[0])
 
 import os
 import torch
