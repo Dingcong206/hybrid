8816f6bd4ff501af01180da0ba58b1be5c5ca85f
Dingcong206 <dingcong882@gmail.com>
2026-01-31 19:42:50 +0900

    D

--- tools/train1.py
+++ tools/train1.py
@@ -86,7 +86,7 @@ class FocalLoss(nn.Module):
 # 3) 核心训练流程
 # =====================================================
 def train():
-    CSV_PATH = "/data/dingcong/hybrid/Coswara-Data/coswara_hear_patches.csv"
+    CSV_PATH = "/data/dingcong/hybrid/Coswara-Data/coswara_hear_multi_segments.csv"
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # 配置超参数
