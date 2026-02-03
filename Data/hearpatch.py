1de83a62768447784a2777d486870df119f5ac53
Dingcong206 <dingcong882@gmail.com>
2026-02-03 16:49:48 +0900

    D

--- Data/hearpatch.py
+++ Data/hearpatch.py
@@ -103,7 +103,7 @@ def process_subset(subset_dir, save_subset_dir, extract_fn, device_tag):
 def main():
     parser = argparse.ArgumentParser()
     # 输入路径指向你已经划分好的父目录
-    parser.add_argument("--src_root", type=str, default="/data/dingcong/hybrid/icbhi_official_sat_patch_tokens")
+    parser.add_argument("--src_root", type=str, default="/data/dingcong/hybrid/icbhi_official_ast_patch_tokens")
     parser.add_argument("--save_root", type=str, default="/data/dingcong/hybrid/icbhi_hear_patch_128_1024")
     parser.add_argument("--hear_path", type=str, default="/home/guest1/.cache/huggingface/hub/models--google--hear/snapshots/9b2eb2853c426676255cc6ac5804b7f1fe8e563f/event_detector/spectrogram_frontend")
     args = parser.parse_args()
