03bd2f80243b73ed2d7f4f21756157df343638f6
Dingcong206 <dingcong882@gmail.com>
2026-02-03 12:25:53 +0900

    D

--- tools/train1.py
+++ tools/train1.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
+import sys
+from pathlib import Path
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(PROJECT_ROOT))
 import os
 import argparse
 import random
