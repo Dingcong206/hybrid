fcbfbc206a325849e9720ef70b60414ff8a1ce2c
Dingcong206 <dingcong882@gmail.com>
2026-05-24 22:07:00 +0900

    DD

--- tools/test.py
+++ tools/test.py
@@ -2,27 +2,28 @@ import sys
 import os
 import torch
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# 把项目根目录加入 Python 搜索路径
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, PROJECT_ROOT)
 
-from models.full_model import TimeFrequencyLogisticModel
+from mymodels.model import TimeFrequencyEncoder
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = TimeFrequencyLogisticModel(
+model = TimeFrequencyEncoder(
     token_dim=768,
     freq_patches=12,
     time_patches=79,
     time_depth=2,
     freq_depth=2,
     num_heads=8,
-    dropout=0.1,
-    num_classes=4
+    dropout=0.1
 ).to(device)
 
 x = torch.randn(2, 948, 768).to(device)
 
-logits = model(x)
+feature = model(x)
 
 print("input shape:", x.shape)
-print("logits shape:", logits.shape)
+print("feature shape:", feature.shape)
